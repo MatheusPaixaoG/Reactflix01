e81e9cc03c6ac9d7d6393d36b012238c84fcec25
TR369 <mpgs@cin.ufpe.br>
2020-07-28 21:36:04 -0300

    comit06

--- src/componentes/Button/index.js
+++ src/componentes/Button/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styled from "styled-components";
 
 /* Se um dia eu precisar usar, dentro do Styled component, um Link, eu precisaria substituir o "button"
@@ -28,29 +27,4 @@ const Button = styled.button`
   }
 `;
 
-=======
-import styled from "styled-components";
-
-const Button = styled.button`
-    color: var(--white);
-    border: 1px solid var(--white);
-    box-sizing: border-box;
-    cursor: pointer;
-    padding: 16px 24px;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 16px;
-    outline: none;
-    border-radius: 5px;
-    text-decoration: none;
-    display: inline-block;
-    transition: opacity .3s;
-    
-    &:hover,
-    &:focus {
-        opacity: .5;
-  }
-`;
-
->>>>>>> fee2e38535e61e329aa2e65d554cd14c4a1ad01b
 export default Button;
