0e094a9a09757257fe510e092168cc271c17e0d9
TR369 <mpgs@cin.ufpe.br>
2020-07-28 21:13:30 -0300

    comit04

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
@@ -21,27 +20,4 @@ function Menu() {
 }
 
 
-=======
-import React from 'react';
-import Logo from '../../assets/img/Logo.png';
-import './Menu.css';
-import Button from '../Button';
-//import ButtonLink from './componentes/ButtonLink';
-
-function Menu() {
-    return (
-        <nav className="Menu">
-            <a href="/">
-                <img className="Logo" src={Logo} alt="Logo Reactflix" />
-            </a>
-
-            <Button as= "a" className="ButtonLink" href="/">
-                Novo vídeo
-            </Button>
-        </nav>
-    );
-}
-
-
->>>>>>> fee2e38535e61e329aa2e65d554cd14c4a1ad01b
 export default Menu; 
