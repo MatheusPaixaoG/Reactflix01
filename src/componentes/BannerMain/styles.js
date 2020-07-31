9cae6324335aa8075c3c3ea78af19a1fac883e7b
TR369 <mpgs@cin.ufpe.br>
2020-07-31 17:49:12 -0300

    comit12

--- src/componentes/BannerMain/styles.js
+++ src/componentes/BannerMain/styles.js
@@ -101,24 +101,24 @@ export const BannerMainContainer = styled.section`
 `;
 
 export const WatchButton = styled.button`
-  background: var(--white);
-  border: 1px solid transparent;
-  border-color: var(--black);
-  border-radius: 5px;
-  box-sizing: border-box;
-  color: var(--black);
-  cursor: pointer;
-  display: none;
-  display: inline-block;
   font-family: 'Roboto', sans-serif;
-  font-style: normal;
-  font-size: 16px;
-  font-weight: bold;
-  margin: 0 auto;
-  outline: none;
+  box-sizing: border-box;
+  cursor: pointer;
   padding: 16px 24px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  outline: none;
+  border-radius: 5px;
   text-decoration: none;
+  display: inline-block;
+  border: 1px solid transparent;
+  color: var(--black);
+  background: var(--white);
+  border-color: var(--black);
   transition: opacity .3s;
+  display: none;
+  margin: 0 auto;
   @media (max-width: 800px) {
     display: block;
   }
